9dfcb66396744ca38eaafb0fa8c94d6adb44f25f
Antonio Gonzalez <anjegonz@gmail.com>
2025-06-12 12:15:20 +0200

    Update: colors in ContentWithMessageBar

--- feature/auth/src/commonMain/kotlin/com/nutrisport/auth/AuthScreen.kt
+++ feature/auth/src/commonMain/kotlin/com/nutrisport/auth/AuthScreen.kt
@@ -24,8 +24,11 @@ import com.nutrisport.shared.Alpha
 import com.nutrisport.shared.BebasNeueFont
 import com.nutrisport.shared.FontSize
 import com.nutrisport.shared.Surface
+import com.nutrisport.shared.SurfaceBrand
+import com.nutrisport.shared.SurfaceError
 import com.nutrisport.shared.TextPrimary
 import com.nutrisport.shared.TextSecondary
+import com.nutrisport.shared.TextWhite
 import rememberMessageBarState
 
 @Composable
@@ -43,7 +46,11 @@ fun AuthScreen() {
                 ),
             messageBarState = messageBarState,
             errorMaxLines = 2,
-            contentBackgroundColor = Surface
+            contentBackgroundColor = Surface,
+            successContainerColor = SurfaceBrand,
+            successContentColor = TextPrimary,
+            errorContainerColor = SurfaceError,
+            errorContentColor = TextWhite
         ) {
             Column(
                 modifier = Modifier
