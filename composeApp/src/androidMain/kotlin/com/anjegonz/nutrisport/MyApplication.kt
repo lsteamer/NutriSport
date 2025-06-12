06d184a23fd291707fbf44641845fb9279717d26
Antonio Gonzalez <anjegonz@gmail.com>
2025-06-12 14:03:25 +0200

    FEATURE: included koin DI

--- composeApp/src/androidMain/kotlin/com/anjegonz/nutrisport/MyApplication.kt
+++ composeApp/src/androidMain/kotlin/com/anjegonz/nutrisport/MyApplication.kt
@@ -3,10 +3,17 @@ package com.anjegonz.nutrisport
 import android.app.Application
 import com.google.firebase.Firebase
 import com.google.firebase.initialize
+import com.nutrisport.di.initializeKoin
+import org.koin.android.ext.koin.androidContext
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        initializeKoin(
+            config = {
+                androidContext(this@MyApplication)
+            }
+        )
         Firebase.initialize(context = this)
     }
 }
--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -2,10 +2,9 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.google.services)
 }
 
 kotlin {
@@ -14,26 +13,19 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "ComposeApp"
+            baseName = "di"
             isStatic = true
         }
     }
-    
+
     sourceSets {
-        
-        androidMain.dependencies {
-            implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
-            implementation(libs.splash.screen)
-            implementation(libs.koin.android)
-        }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
@@ -43,43 +35,24 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation(libs.auth.kmp)
-            implementation(libs.firebase.app)
 
-            implementation(project(path = ":navigation"))
-            implementation(project(path = ":shared"))
-            implementation(project(path = ":di"))
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
         }
     }
 }
 
 android {
-    namespace = "com.anjegonz.nutrisport"
+    namespace = "com.anjegonz.di"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.anjegonz.nutrisport"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-}
-
-dependencies {
-    debugImplementation(compose.uiTooling)
 }
