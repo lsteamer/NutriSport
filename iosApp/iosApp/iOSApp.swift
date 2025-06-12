9eeb312dd814e596ecc4ad1acfc8c17da9d8228e
Antonio Gonzalez <anjegonz@gmail.com>
2025-06-12 12:08:08 +0200

    Update: Firebase Auth iOS modification

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -17,26 +17,11 @@ struct iOSApp: App {
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         FirebaseApp.configure()
         return true
-    }
-    
-    func application(
-        _ application: UIApplication,
-        open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-    ) -> Bool {
-  
-        var handled: Bool
-        
-        handled = GIDSignIn.sharedInstance.handle(url)
-        if handled {
-            return true
-        }
-        return false
     }
 }
