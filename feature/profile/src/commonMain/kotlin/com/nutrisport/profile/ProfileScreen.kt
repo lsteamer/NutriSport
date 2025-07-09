834517d0088730a62865c80beb35647ea52a5193
Antonio Gonzalez <lsteamer@gmail.com>
2025-07-09 15:22:26 +0200

    Compose: Created ProfileForm

--- feature/profile/src/commonMain/kotlin/com/nutrisport/profile/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/com/nutrisport/profile/ProfileScreen.kt
@@ -1,9 +1,24 @@
 package com.nutrisport.profile
 
 import androidx.compose.runtime.Composable
+import com.nutrisport.shared.component.ProfileForm
 
 
 @Composable
 fun ProfileScreen() {
-
+    ProfileForm(
+        firstName = "John",
+        onFirstNameChange = {},
+        lastName = "Doe",
+        onLastNameChange = {},
+        email = "james.c.mcreynolds@example-pet-store.com",
+        city = "New York",
+        onCityChange = {},
+        postalCode = 10001,
+        onPostalCodeChange = {},
+        address = "123 Main St",
+        onAddressChange = {},
+        phoneNumber = "",
+        onPhoneNumberChange = {}
+    )
 }
---
+++ shared/src/commonMain/kotlin/com/nutrisport/shared/component/ProfileForm.kt
@@ -0,0 +1,104 @@
+package com.nutrisport.shared.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.nutrisport.shared.Strings
+import com.nutrisport.shared.Surface
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun ProfileForm(
+    modifier: Modifier = Modifier,
+    firstName: String,
+    onFirstNameChange: (String) -> Unit,
+    lastName: String,
+    onLastNameChange: (String) -> Unit,
+    email: String,
+    city: String,
+    onCityChange: (String) -> Unit,
+    postalCode: Int?,
+    onPostalCodeChange: (Int?) -> Unit,
+    address: String,
+    onAddressChange: (String) -> Unit,
+    phoneNumber: String?,
+    onPhoneNumberChange: (String?) -> Unit,
+) {
+
+    Box(
+        modifier = Modifier
+            .background(Surface)
+            .systemBarsPadding()
+    ) {
+        Column(
+            modifier
+                .fillMaxSize()
+                .padding(
+                    horizontal = 24.dp,
+                    vertical = 12.dp
+                )
+                .verticalScroll(state = rememberScrollState())
+                .imePadding(),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            CustomTextField(
+                value = firstName,
+                onValueChange = onFirstNameChange,
+                placeholder = stringResource(Strings.firstName),
+                error = firstName.length !in 3..50
+            )
+            CustomTextField(
+                value = lastName,
+                onValueChange = onLastNameChange,
+                placeholder = stringResource(Strings.lastName),
+                error = lastName.length !in 3..50
+            )
+            CustomTextField(
+                value = email,
+                onValueChange = { },
+                placeholder = stringResource(Strings.email),
+                enabled = false
+            )
+            CustomTextField(
+                value = city,
+                onValueChange = onCityChange,
+                placeholder = stringResource(Strings.city),
+                error = city.length !in 3..50
+            )
+            CustomTextField(
+                value = "${postalCode ?: ""}",
+                onValueChange = { onPostalCodeChange(it.toIntOrNull()) },
+                placeholder = stringResource(Strings.postalCode),
+                error = postalCode.toString().length !in 3..7
+            )
+            CustomTextField(
+                value = address,
+                onValueChange = onAddressChange,
+                placeholder = stringResource(Strings.address),
+                error = address.length !in 3..50
+            )
+            CustomTextField(
+                value = phoneNumber ?: "",
+                onValueChange = onPhoneNumberChange,
+                placeholder = stringResource(Strings.phoneNumber),
+                error = phoneNumber.toString().length !in 3..15
+            )
+        }
+
+
+    }
+}
+
